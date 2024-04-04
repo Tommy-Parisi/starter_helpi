4f65fe32f279b2d380f732e11f087e9b2c3e2aab
Tommy-Parisi <tparisi@udel.edu>
2024-04-04 13:07:11 -0400

    Added navigation between home and basic questions pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,4 +2,21 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
+const Home = () => {
+    const navigate = useNavigate();
+
+    const toBasicQuestions = () => {
+        navigate('/BasicQuestions');
+    };
+
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <Button onClick={toBasicQuestions}>Basic Questions</Button>
+        </div>
+    );
+}
+
+export default Home;
