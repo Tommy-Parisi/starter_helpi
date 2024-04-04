e7ef495005ca7097f69e4f83ff3da2edbd2cb08a
Tommy-Parisi <tparisi@udel.edu>
2024-04-04 13:00:54 -0400

    Added routes to App.tsx

---
+++ src/pages/BasicQuestions.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button, Form } from 'react-bootstrap';
+
+<div className = "BasicQuestions">
+    <header className="BasicQuestions-header"> 
+    Header Text Here
+    </header>
+
+
+</div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,3 +2,4 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+
