f3b5e76be797e1704d38c099c92e16a5d4fe325e
Tommy-Parisi <tparisi@udel.edu>
2024-04-04 13:09:15 -0400

    Basic question page setup

--- src/pages/BasicQuestions.tsx
+++ src/pages/BasicQuestions.tsx
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 
-<div className = "BasicQuestions">
-    <header className="BasicQuestions-header"> 
-    Header Text Here
-    </header>
+const BasicQuestions = () => {
+    return (
+        <div className = "BasicQuestions">
+            <header className="BasicQuestions-header"> Header Text Here </header>
+        </div>
+    );
+}
 
-
-</div>
+export default BasicQuestions;
