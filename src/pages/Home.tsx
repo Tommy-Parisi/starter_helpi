bd2ce0c068a3c539402804ce97b6c067f1512b8d
Tommy-Parisi <tparisi@udel.edu>
2024-04-04 13:15:09 -0400

    Made home default screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import { Button, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
