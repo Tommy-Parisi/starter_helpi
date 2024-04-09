309a524923f4245079a7764e4293c77e52fa2f02
Tommy-Parisi <tparisi@udel.edu>
2024-04-09 13:59:10 -0400

    Added some color to the home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,9 +12,10 @@ const Home: React.FC<HomeProps> = ({ changePage }) => {
             display: 'flex',
             padding: '10px',
             fontSize: '50px',
-            backgroundColor: 'blue',
+            backgroundColor: '#273469',
             width: `100%`,
             height: '50px',
+            color: '#E4D9FF'
         },
         container: {
             display: 'flex',
@@ -33,24 +34,30 @@ const Home: React.FC<HomeProps> = ({ changePage }) => {
             marginTop: '0px',
             width: '100%',
             display: 'flex',
+            backgroundColor: '#E4D9FF',
+        },
+        customButton: {
+            backgroundColor: '#1E2749',
+            color: '#E4D9FF',
         }
     }
 
     return(
         <>
         <div style={style.header}>Home</div>
+        
         <div style={style.page}>
 
             <div style={style.column}>
                 <h2>Basic Career Questions</h2>
                 <p>Kickstart your career exploration with our Basic Career Questions! Designed to touch upon the surface aspects of various professions, these questions aim to quickly gauge your general interests, educational background, and work preferences. Perfect for those at the start of their career journey, these questions provide a straightforward way to discover potential career paths without delving into the complexities of each field.</p>
-                <Button onClick={() => changePage('Basic')}>Basic Questions</Button>
+                <Button style={style.customButton} onClick={() => changePage('Basic')}>Basic Questions</Button>
             </div>
 
             <div style={style.column}>
                 <h2>Detailed Career Questions</h2>
                 <p>Ready to dive deeper into your career exploration? Our Detailed Career Questions are designed to provide a comprehensive overview of various professions, including the skills required, the educational background needed, and the day-to-day responsibilities of each job. Ideal for those who have a general idea of their career interests and are looking to explore potential career paths in more detail.</p>
-                <Button onClick={() => changePage('Detailed')}>Detailed Questions</Button>
+                <Button style={style.customButton} onClick={() => changePage('Detailed')}>Detailed Questions</Button>
             </div>
 
         </div>
