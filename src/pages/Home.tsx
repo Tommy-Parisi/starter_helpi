5ff3e35302de4dcc750c822189a0f602fea91ddc
Tommy-Parisi <tparisi@udel.edu>
2024-04-11 23:09:35 -0400

    Better solution to home screen buttons, actual buttons now

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import { Button } from 'react-bootstrap';
 
 interface HomeProps {
     changePage: (page: string) => void;
@@ -11,19 +12,28 @@ const Home: React.FC<HomeProps> = ({ changePage }) => {
         <div className="page">
 
             <div className="column">
-                <div className="container" onClick={() => changePage('Basic')}>
+
+                <Button className="customButton" onClick={() => changePage('Basic')}>
+
+                
                     <h2>The Basic Quiz</h2>
                     <h3>(5 Mins)</h3>
                     <p>Kickstart your career exploration with our Basic Career Questions! Designed to touch upon the surface aspects of various professions, these questions aim to quickly gauge your general interests, educational background, and work preferences. Perfect for those at the start of their career journey, these questions provide a straightforward way to discover potential career paths without delving into the complexities of each field.</p>
-                </div>
+             
+
+                </Button>
+
             </div>
 
             <div className="column">
-                <div className="container" onClick={() => changePage('Detailed')}>
+
+                <Button className='customButton' onClick={() => changePage('Detailed')}>
+                
                     <h2>The Detailed Quiz</h2>
                     <h3>(10 Mins)</h3>
                     <p>Ready to dive deeper into your career exploration? Our Detailed Career Questions are designed to provide a comprehensive overview of various professions, including the skills required, the educational background needed, and the day-to-day responsibilities of each job. Ideal for those who have a general idea of their career interests and are looking to explore potential career paths in more detail.</p>
-                </div>
+                
+                </Button>
 
             </div>
 
