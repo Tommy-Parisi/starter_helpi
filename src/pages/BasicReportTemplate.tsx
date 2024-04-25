068eeb0130c2be9457477b86d5749909064f36f1
Ellie Dassoulas <ellied@udel.edu>
2024-04-25 10:00:09 -0400

    made first styling changes

--- src/pages/BasicReportTemplate.tsx
+++ src/pages/BasicReportTemplate.tsx
@@ -11,22 +11,25 @@ const BasicReport: React.FC<BasicReportProps> = ({ changePage }) => {
         </div>
 
         <div className="pageBody">
-        <div className='container'>
-            <div className="column">
-                <div className = "generalInfoBasic">
+        <div className='containerReportPage'>
+        <div className="column">
+            <div className="customButtonReportPages">
                     <h1>General Information</h1>
                     <p>Based on your answers, you showed the following raits of people in industries....</p>
                 </div>
             </div>
+            </div>
 
             <div className="column">
-                <div className = "recIndustriesBasic">
+            <div className='containerReportPage'>
+            <div className="customButtonReportPages">
+                {/*<div className = "recIndustriesBasic">*/}
                     <h1 className='recBasicHeader'>Recommended Industries</h1>
                     <p className="option1">Option 1: ...</p>
                    <p className="learn">Learn More</p>
                 </div>
             </div>
-        </div>
+            </div>
         </div>
         </>
         );
